fdc62045be062c45659227da141ff08a93a54980
GonzaloDP <gondap@outlook.com>
2026-09-15 19:31:09 -0300

    Cree una escena de Recurso junto con su Script

---
+++ proto-team-2/Scripts/recurso.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+#Este "enum" sirve para enumerar constantes del "0"(siendo el primer parametro) hasta el último parametro(en este caso el "3"), es para no hacer varias escenas de lo mismo y resumirlo en una sola lista
+enum Tipo_Recurso{ MADERA, PIEDRA, COMIDA, METAL}
+
+#Cantidad con la que van a iniciar los recursos(quizás más adelante cambie a ser random)
+@export var cantidad_inicial = 100
+
+var tipo: Tipo_Recurso#Variable del tipo de lo que contenga la lista
+var cantidad_variable : int#Cantidad de recurso que va a mostrar cuando nos diga la información de cada uno
+
+func _ready() -> void:
+	tipo = Tipo_Recurso.values().pick_random()
+	cantidad_variable = cantidad_inicial
+
+func _process(delta: float) -> void:
+	pass
