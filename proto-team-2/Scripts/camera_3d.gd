5217ef850e1bc3a86a2263f966aa983139177b52
GonzaloDP <gondap@outlook.com>
2026-09-16 16:29:50 -0300

    Selección y des elección de unidad y unidades

--- proto-team-2/Scripts/camera_3d.gd
+++ proto-team-2/Scripts/camera_3d.gd
@@ -1,8 +1,35 @@
 extends Camera3D
 
-@export var unidad: CharacterBody3D
+@onready var selection_box = $"../UI/SelectionBox"
+
+#@export var unidad: CharacterBody3D
+
+var selected_units: Array[CharacterBody3D] = []
+var seleccionando: bool = false
+var posicion_inicio_seleccion: Vector2
+var posicion_actual_mouse: Vector2
 
 func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.is_pressed():
+			seleccionando = true
+			posicion_inicio_seleccion = event.position
+			posicion_actual_mouse = event.position
+			
+			selection_box.actualizar_rectangulo(posicion_inicio_seleccion, posicion_actual_mouse)
+		else:
+			if posicion_inicio_seleccion == posicion_actual_mouse:
+				seleccionar_unidad(posicion_actual_mouse)
+			else:
+				seleccionar_unidades_en_rectangulo()
+			
+			seleccionando = false
+			selection_box.ocultar_rectangulo()
+		
+	if event is InputEventMouseMotion and seleccionando:
+		posicion_actual_mouse = event.position
+		selection_box.actualizar_rectangulo(posicion_inicio_seleccion, posicion_actual_mouse)
+		
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
 		raycast_to_ground(event.position)
 
@@ -18,5 +45,36 @@ func raycast_to_ground(mouse_pos: Vector2) -> void:
 	if result:
 		var click_world_position: Vector3 = result.position
 		
-		if is_instance_valid(unidad):
-			unidad.set_move_target(click_world_position)
+		for unidad in selected_units:
+			if is_instance_valid(unidad):
+				unidad.set_move_target(click_world_position)
+
+func seleccionar_unidad(mouse_pos: Vector2) -> void:
+	selected_units.clear()
+	
+	var space_state = get_world_3d().direct_space_state
+	var ray_origin = project_ray_origin(mouse_pos) 
+	var ray_end = ray_origin + project_ray_normal(mouse_pos) * 1000.0
+	
+	var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_end)
+	var result = space_state.intersect_ray(query)
+	
+	if result:
+		var objeto = result.collider
+		
+		if objeto is CharacterBody3D:
+			selected_units.append(objeto)
+			print(selected_units)
+
+func seleccionar_unidades_en_rectangulo() -> void:
+	selected_units.clear()
+	
+	var rectangulo = Rect2(posicion_inicio_seleccion, posicion_actual_mouse - posicion_inicio_seleccion).abs()
+	var unidades = get_tree().get_nodes_in_group("unidades")
+	
+	for unidad in unidades:
+		var posicion_pantalla = unproject_position(unidad.global_position)
+		
+		if rectangulo.has_point(posicion_pantalla):
+			selected_units.append(unidad)
+			print(selected_units)
